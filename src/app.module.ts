e0ec7b1bb04d7d00737e154df02ae17a6aefb040
Ferrel John Fernando <ferreluchiha@gmail.com>
2022-01-20 21:31:25 +0700

    add validation pipe and its dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      ssl: false,
+      synchronize: true,
+    }),
+    PetsModule,
+  ],
   controllers: [],
   providers: [],
 })
